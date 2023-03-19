e45579cedc155d242e56554b07e3d28c35596918
Valkon <nazoltejwww@interia.pl>
2023-03-19 21:49:13 +0100

    Update kontakt.js

--- src/pages/kontakt.js
+++ src/pages/kontakt.js
@@ -40,7 +40,7 @@ const Kontakt = () => {
             .then(
                 () =>
                     (window.location.href =
-                        "https://nazoltejwww.netlify.app/dziekujemy/")
+                        "https://nazoltej.netlify.app/dziekujemy/")
             )
             .catch((error) => alert(error));
 
